f9b2e450b1292e808aeeb5e19a1b1c6ed4033dbb
Justin Mcconnell <jjppmm29@hotmail.com>
2017-07-11 13:06:26 -0600

    Post testing fixes

--- commands.js
+++ commands.js
@@ -947,14 +947,18 @@ function regCommands(commandManager) {
                 utils.BOT.sendChat(utils.getTargetName() + ' Songs you can\'t or shouldn\'t play: https://git.io/vHDjH');
             }
         ),
-		new Command('emotesonly',['toggleemotesonly','emotesonly'],0,['mod'],[],
+		new Command('emotesonlymode',['toggleemotesonly','emotesonly'],1,['mod'],[],
 			function(utils){
-				utils.settingsManager.setEmoteOnlyMode(!utils.getEmoteOnlyMode());
 				if(utils.settingsManager.getEmoteOnlyMode())
 				{
-					//fine for now
-					//but i need a better way TBH
+					utils.BOT.sendChat("Emotes Only Mode Off");
+					utils.settingsManager.setEmoteOnlyMode(false);
+				}
+				else
+				{
+					utils.BOT.sendChat("Emotes Only Mode On");
 					var enableAbsoluteEmotes = utils.getCommandArguments()[0];
+					
 					if(enableAbsoluteEmotes === 'absolute')
 					{
 						utils.settingsManager.setEmoteAbsolute(true);
@@ -963,6 +967,7 @@ function regCommands(commandManager) {
 					{
 						utils.settingsManager.setEmoteAbsolute(false);
 					}
+					utils.settingsManager.setEmoteOnlyMode(true);
 				}
 			}
 		),
